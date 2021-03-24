7b022a81694012248d9bdc24400e936c3b28dc96
weilan_wu <weilan_wu@apple.com>
2021-03-24 07:59:30 -0700

    continued dev

--- python/BaseJenkinsJob.py
+++ python/BaseJenkinsJob.py
@@ -32,53 +32,83 @@ class BaseJenkinsJob:
 
     def copy_job(self, job_name, new_job_name):
         self._server.copy_job(job_name, new_job_name)
-
-    def create(self, config_xml):
-        """
-        Create a job
-        :param str config_xml: XML configuration of new job
-        """
-        if not config_xml:
-            raise JenkinsAPIException('Job XML config cannot be empty')
-
-        params = {'name': self._name}
-        self._server.get_requester().post_xml_and_confirm_status(
-            self._server.get_create_url(),
-            data=config_xml,
-            params=params
-        )
-        print("The job {} has been successfully created".format(self._name))
+    #
+    # def create(self, config_xml):
+    #     """
+    #     Create a job
+    #     :param str config_xml: XML configuration of new job
+    #     """
+    #     if not config_xml:
+    #         raise JenkinsAPIException('Job XML config cannot be empty')
+    #
+    #     params = {'name': self._name}
+    #     self._server.get_requester().post_xml_and_confirm_status(
+    #         self._server.get_create_url(),
+    #         data=config_xml,
+    #         params=params
+    #     )
+    #     print("The job {} has been successfully created".format(self._name))
 
 
 # test driver
 if __name__ == "__main__":
-    if len(sys.argv) < 4:
-        print(
-            "BaseJenkinsJob.py <url> <username> <password> <job-name> [--clone | --create]\n")
-        sys.exit(-1)
+    if len(sys.argv) < 5:
+        sys.exit("BaseJenkinsJob.py <url> <username> <password> <job-name> "
+                 "[--clone | --create | --trigger]\n")
     url = sys.argv[1]
+    base_jenkins = BaseJenkinsAPI(url, sys.argv[2], sys.argv[3])
     job_name = sys.argv[4]
     op = sys.argv[5] if len(sys.argv) >= 6 else None
-    base_jenkins = BaseJenkinsAPI(sys.argv[1], sys.argv[2], sys.argv[3])
-    # base_jenkins_job = BaseJenkinsJob(base_jenkins, job_name)
+
     if op and op == "--clone":
+        """
+           python3 -m BaseJenkinsJob http://localhost:8080 admin \
+                `cat jenkins-api-token.txt` git_webhook_demo --clone
+        """
         if not base_jenkins.has_job(job_name):
-            sys.exit(
-                "Job {} is not a valid job name on server {}".format(job_name, url))
-        new_job = input(
-            "Please give the new job name to clone from job {} : ".format(sys.argv[3]))
-        base_jenkins_job.copy_job(job_name, new_job)
+            sys.exit("Job {} is not a valid job name on server {}"
+                     .format(job_name, url))
+        new_job = input("Please give the new job name to clone from job {} : "
+                        .format(job_name))
+        if base_jenkins.has_job(new_job):
+            sys.exit("Job name '{}' already present in the Jenkins server, "
+                     "please choose a different job name."
+                     .format(new_job))
+        try:
+            base_jenkins_job = BaseJenkinsJob(base_jenkins, job_name)
+            base_jenkins_job.copy_job(job_name, new_job)
+        except JenkinsAPIException as je:
+            print("caught exception: {}".format(je))
+        print("Successfully cloned job from {}: {}".format(job_name, new_job))
+
     elif op and op == "--create":
+        """
+           python3 -m BaseJenkinsJob http://localhost:8080 admin \
+                `cat jenkins-api-token.txt` git_webhook_demo-new --create config.xml
+        """
         if base_jenkins.has_job(job_name):
             sys.exit("Job {} is already present on server {}, use a different name."
                      .format(job_name, url))
         if len(sys.argv) < 7:
             sys.exit("Missing input config xml file for JJ create.")
+
         with open(sys.argv[6], "r") as input_file:
             config_xml = input_file.read()
-        base_jenkins_job = BaseJenkinsJob(base_jenkins, job_name)
-        base_jenkins_job.create(config_xml)
+
+        base_jenkins_job = base_jenkins.create_job(job_name, config_xml)
+
+    elif op and op == "--trigger":
+        """
+           python3 -m BaseJenkinsJob http://localhost:8080 admin \
+                `cat jenkins-api-token.txt` git_webhook_demo --trigger
+        """
+        base_jenkins.build_job(job_name)
+
     else:
+        """
+           python3 -m BaseJenkinsJob http://localhost:8080 admin \
+                  `cat jenkins-api-token.txt` git_webhook_demo config.xml
+        """
         base_jenkins_job = BaseJenkinsJob(base_jenkins, job_name)
         config_xml = base_jenkins_job.get_config_xml()
         if op:
