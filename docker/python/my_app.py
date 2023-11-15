1e4aeed4bd311b1e8eb8e25ade879b31f937b0b6
bluecoder008 <bluecoder008@gmail.com>
2023-11-15 14:16:11 -0800

    updated my_app.py to latest

--- docker/python/my_app.py
+++ docker/python/my_app.py
@@ -1,10 +1,14 @@
 from flask import Flask, __version__ 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/ok')
 def hello_world():
    return f'Hello World (flask version {__version__})'
 
+@app.route('/')
+def hello_world0():
+   return 'Hello World'
+
 if __name__ == '__main__':
-   app.run()
+   app.run(host='0.0.0.0')
 
